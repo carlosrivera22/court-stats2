360676193b7ec1e8c5548ce5f62118c090fc41d1
Carlos Geronimo <cgeronimo.rivera22@gmail.com>
2023-10-31 09:08:33 -0400

    adding the player controller

---
+++ backend/src/players/players.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('players')
+export class PlayersController {
+  @Get()
+  sayHello(): string {
+    return 'Hello from NestJS!';
+  }
+}
