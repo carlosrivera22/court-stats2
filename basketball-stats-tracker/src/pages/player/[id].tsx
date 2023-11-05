b91bd99f7fbc07ddca785898b592957fc587f4ba
Carlos Geronimo <cgeronimo.rivera22@gmail.com>
2023-11-05 16:02:20 -0400

    clickable profile picture

--- basketball-stats-tracker/src/pages/player/[id].tsx
+++ basketball-stats-tracker/src/pages/player/[id].tsx
@@ -1,16 +1,18 @@
 import { useRouter } from "next/router";
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Box, Button, Typography } from "@mui/material";
 import ProfileTabs from "@/components/ProfileTabs";
 import { getPlayer } from "../../services/players";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const PlayerProfilePage = () => {
   const router = useRouter();
   const { id } = router.query;
   const [player, setPlayer] = useState<any>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null); // Reference to the file input
+
   const getPlayerById = async (playerId: number) => {
     const data = await getPlayer(playerId);
-    setPlayer(data); // Set the player data directly here
+    setPlayer(data);
   };
 
   useEffect(() => {
@@ -19,18 +21,40 @@ const PlayerProfilePage = () => {
     }
   }, [id]);
 
+  const handleAvatarClick = () => {
+    // Trigger file input click when Avatar is clicked
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    // Handle the file upload here
+    console.log("Selected file:", file);
+    // You can call an upload function here
+  };
+
   if (!player) return null;
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center" padding={5}>
-      <Avatar
-        src="/images/player_placeholder.png"
-        sx={{ width: 150, height: 150, marginBottom: 5, marginTop: 1 }}
+      <input
+        type="file"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        style={{ display: "none" }}
+        accept="image/*" // Accept only image files
       />
-      <Typography variant="h4" gutterBottom>
+      <Button onClick={handleAvatarClick}>
+        <Avatar
+          src="/images/player_placeholder.png"
+          sx={{ width: 150, height: 150, marginBottom: 2, marginTop: 1 }}
+        />
+      </Button>
+      <Typography variant="h5" gutterBottom>
         {player.firstName} {player.lastName}
       </Typography>
-      {/* monitor prop drilling */}
       <ProfileTabs player={player} refetchPlayer={getPlayerById} />
     </Box>
   );
