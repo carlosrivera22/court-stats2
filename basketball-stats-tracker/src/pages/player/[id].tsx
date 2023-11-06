1cd482ffad2836405eaac18623bb77ed29dc0794
Carlos Geronimo <cgeronimo.rivera22@gmail.com>
2023-11-06 07:18:18 -0400

    loading

--- basketball-stats-tracker/src/pages/player/[id].tsx
+++ basketball-stats-tracker/src/pages/player/[id].tsx
@@ -5,12 +5,14 @@ import { getPlayer, updatePlayer } from "../../services/players";
 import { useEffect, useState, useRef } from "react";
 import { storage } from "@/firebase/firebase";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const PlayerProfilePage = () => {
   const router = useRouter();
   const { id } = router.query;
   const [player, setPlayer] = useState<any>(null);
   const fileInputRef = useRef<HTMLInputElement>(null); // Reference to the file input
+  const [loading, setLoading] = useState(false);
 
   const getPlayerById = async (playerId: number) => {
     const data = await getPlayer(playerId);
@@ -35,6 +37,7 @@ const PlayerProfilePage = () => {
     const storageRef = ref(storage, `profileImages/${file.name}`);
 
     try {
+      setLoading(true);
       // Upload the file to Firebase Storage
       const snapshot = await uploadBytes(storageRef, file);
 
@@ -43,8 +46,10 @@ const PlayerProfilePage = () => {
       await updatePlayer(player.id, {
         profilePictureUrl: url,
       });
+      setLoading(false);
       return url; // This URL can be saved to your database
     } catch (error) {
+      setLoading(false);
       console.error("Error uploading file:", error);
       // Handle errors here
     }
@@ -70,12 +75,18 @@ const PlayerProfilePage = () => {
         style={{ display: "none" }}
         accept="image/*" // Accept only image files
       />
-      <Button onClick={handleAvatarClick}>
-        <Avatar
-          src={player.profilePictureUrl ?? "/images/player_placeholder.png"}
-          sx={{ width: 150, height: 150, marginBottom: 2, marginTop: 1 }}
-        />
-      </Button>
+      {loading ? (
+        <Box mt={2} mb={2}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Button onClick={handleAvatarClick}>
+          <Avatar
+            src={player.profilePictureUrl ?? "/images/player_placeholder.png"}
+            sx={{ width: 150, height: 150, marginBottom: 2, marginTop: 1 }}
+          />
+        </Button>
+      )}
       <Typography variant="h5" gutterBottom>
         {player.firstName} {player.lastName}
       </Typography>
